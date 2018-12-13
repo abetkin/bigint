5801eea7b9882933726d8ed00a5f036c0796703d
Vitalii Abetkin <abvit89@gmail.com>
2018-12-14 01:55:13 +0300

    tmp

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,19 @@ impl Number {
         let chunks: Vec<_> = n.chunks.iter().map(|n| n.to_string()).collect();
         chunks.join("")
     }
+
+    fn equate_sizes(&mut self, &mut other: Number) {
+        let delta = (self.chunks.len() - other.chunks.len()) as i32;
+        if delta < 0 {
+            for i in 0..-delta {
+                self.chunks.push(0);
+            }
+        } else {
+            for i in 0..delta {
+                other.chunks.push(0);
+            }
+        }
+    }
 }
 
 
@@ -26,6 +39,7 @@ impl Add for Number {
 
     fn add(self, other: Number) -> Number {
         let pairs = self.chunks.iter().zip(other.chunks.iter());
+        Number::equate_sizes(&mut self, other);
         let mut result = vec![];
         let mut overflow = false;
         for (x1, x2) in pairs {
@@ -47,29 +61,43 @@ impl Add for Number {
     }
 }
 
-fn fibonacci_fast(n: i32) -> Number {
-    if n < 2 {
-        Number::new(n)
-    } else {
-        fib(n)
-    }
-}
+// TODO how number is split in chunks?
 
-fn fib(n: i32) -> Number {
+// fn fibonacci_fast(n: i32) -> Number {
+//     if n < 2 {
+//         Number::new(n)
+//     } else {
+//         fib(n)
+//     }
+// }
 
-    let fib_raw = "array"; // FIXME
-        let new_next = next + prev;
-        prev = next;
-        next = new_next;
-    }
-    next
+// fn fib(n: i32) -> Number {
+//     // TODO
+//     let mut result: Vec<Number> = Vec::new();
+//     result.push(Number::new(0));
+//     result.push(Number::new(1));
+//     for i in 2 .. (n as usize) + 1 {
+//         result.push(result[i-1] + result[i-2]);
+//     }
+//     result[n as usize]
+// }
+
+fn test_sum() -> Number{
+    let num1 = Number{
+        chunks: vec![0, 1, 2]
+    };
+    let num2 = Number {
+        chunks: vec![10]
+    };
+    num1 + num2
 }
 
 fn main() {
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input);
-    let n: i32 = input.trim().parse().unwrap();
-    print!("{}\n", Number::format(fibonacci_fast(n)))
-
+    // let mut input = String::new();
+    // std::io::stdin().read_line(&mut input);
+    // let n: i32 = input.trim().parse().unwrap();
+    // print!("{}\n", Number::format(fibonacci_fast(n)))
+    let x = test_sum();
+    print!("{}\n", Number::format(x));
 
 }
